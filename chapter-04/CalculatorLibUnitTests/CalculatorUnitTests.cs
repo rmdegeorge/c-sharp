06c781be398034ffb2dc983ad040b3cc4670d4d1
Rob DeGeorge <rmdegeorge@gmail.com>
2020-08-10 07:33:47 -0600

    chapter 4

--- chapter-04/CalculatorLibUnitTests/CalculatorUnitTests.cs
+++ chapter-04/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -3,12 +3,38 @@ using Xunit;
 
 namespace CalculatorLibUnitTests
 {
-    public class UnitTest1
+    public class CalculatorUnitTests
     {
         [Fact]
-        public void Test1()
+        public void TestAdding2and2()
         {
+            // arrange
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+            var calc = new Calculator();
 
+            // act
+            double actual = calc.Add(a, b);
+
+            // assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAdding2and3()
+        {
+            // arrange
+            double a = 2;
+            double b = 3;
+            double expected = 5;
+            var calc = new Calculator();
+
+            // act
+            double actual = calc.Add(a, b);
+
+            // assert 
+            Assert.Equal(expected, actual);
         }
     }
 }
