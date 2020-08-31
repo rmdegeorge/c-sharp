0b1ffdc5e73c06f487928bfcaa88cce5f3c1a5ef
Rob DeGeorge <rmdegeorge@gmail.com>
2020-08-31 07:34:02 -0600

    passing parameters to methods

--- chapter-05/PacktLibrary/Person.cs
+++ chapter-05/PacktLibrary/Person.cs
@@ -49,5 +49,40 @@ namespace Packt.Shared
         {
             return (Name: "Apples", Number: 5);
         }
+        public string SayHello()
+        {
+            return $"{Name} says 'Hello!'";
+        }
+        public string SayHello(string name)
+        {
+            return $"{Name} says 'Hello {name}!'";
+        }
+        public string SayHelloTo(string name)
+        {
+            return $"{Name} says 'Hello {name}!'";
+        }
+        public string OptionalParameters(
+            string command = "Run!",
+            double number = 0.0,
+            bool active = true
+        )
+        {
+            return string.Format(
+                format: "command is {0}, number is {1}, active is {2}",
+                arg0: command,
+                arg1: number,
+                arg2: active
+            );
+        }
+        public void PassingParameters(int x, ref int y, out int z)
+        {
+            // out parameters cannot have a default AND must be initialized inside the method
+            z = 99;
+
+            // increment each parameter
+            x++;
+            y++;
+            z++;
+        }
     }
 }
